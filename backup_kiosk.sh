3752dcdd6947575833afbefed2984febc93c26c6
FabLab EDP <fablab@edp.pt>
2023-09-05 17:47:16 +0100

    add trailing slash to local backup dir variable

--- backup_kiosk.sh
+++ backup_kiosk.sh
@@ -15,21 +15,21 @@ fi
 mkdir -p $LOCAL_BACKUP_DIR
 
 echo "Creating local database backup..."
-if cp /home/pi/fablabedp-kiosk/kiosk_app/database.json $LOCAL_BACKUP_DIR/database_$datestamp.json ; then
+if cp /home/pi/fablabedp-kiosk/kiosk_app/database.json "$LOCAL_BACKUP_DIR"database_$datestamp.json ; then
     echo "done."
 else
     echo "backup failed."
 fi
 
 echo "Deleting backups older than 1 week"
-if find  $LOCAL_BACKUP_DIR/ -type f -mtime +7 -delete ; then
+if find  $LOCAL_BACKUP_DIR -type f -mtime +7 -delete ; then
     echo "done."
 else
     echo "delete failed."
 fi
 
 echo "Backing up database backups..."
-if lftp -u "$FTP_USER","$FTP_PASS" -e "mirror -Re $LOCAL_BACKUP_DIR/ database/; quit" ftp://"$FTP_SERVER" ; then
+if lftp -u "$FTP_USER","$FTP_PASS" -e "mirror -Re $LOCAL_BACKUP_DIR database/; quit" ftp://"$FTP_SERVER" ; then
     echo "done."
 else
     echo "backup failed."
